91b56308d744b17b94c74cdd58033461d63936d2
marcelovimelo <marcelovimelo@gmail.com>
2018-12-15 00:24:53 -0200

    imc atividade

--- CS477-2018-02-atividade-pratica-001/js/imc.js
+++ CS477-2018-02-atividade-pratica-001/js/imc.js
@@ -5,8 +5,10 @@
          var IMC = peso / Math.pow(altura, 2);
 
     document.getElementById("output").innerHTML = Math.round(IMC * 100)/100;
-         if (IMC < 18.5) document.getElementById("comment").innerHTML = "Peso baixo";
-         if (IMC >= 18.5 && IMC <= 25) document.getElementById("comment").innerHTML = "Normal";
-         if (IMC >= 25 && IMC <= 30) document.getElementById("comment").innerHTML = "Obeso";
-         if (IMC > 30) document.getElementById("comment").innerHTML = "Obesidade 2";            
+         if (IMC < 18.5) document.getElementById("comment").innerHTML = "Subnutrição";
+         if (IMC >= 18.5 && IMC <= 24.9) document.getElementById("comment").innerHTML = "Peso Saudável";
+         if (IMC >= 25 && IMC <= 29.9) document.getElementById("comment").innerHTML = "Sobrepeso";
+         if (IMC >= 30 && IMC <= 34.9) document.getElementById("comment").innerHTML = "Obesidade grau 1";
+         if (IMC >= 35 && IMC <= 39.9) document.getElementById("comment").innerHTML = "Obesidade grau 2";
+         if (IMC > 40) document.getElementById("comment").innerHTML = "Obesidade grau 3";
      }
